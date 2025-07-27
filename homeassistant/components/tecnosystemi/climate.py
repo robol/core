6eec3a398d74fd823f4a6cb6f830c485997c85b0
Leonardo Robol <leo@robol.it>
2025-07-27 16:12:21 +0200

    Coordinator updating.

--- homeassistant/components/tecnosystemi/climate.py
+++ homeassistant/components/tecnosystemi/climate.py
@@ -9,14 +9,15 @@ from homeassistant.components.climate import (
     ClimateEntityFeature,
     HVACMode,
 )
-from homeassistant.const import CONF_PIN, UnitOfTemperature
-from homeassistant.core import HomeAssistant
+from homeassistant.const import UnitOfTemperature
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from . import TecnosystemiConfigEntry
-from .api import Device
 from .const import DOMAIN
+from .coordinator import TecnosystemiCoordinator
 
 
 async def async_setup_entry(
@@ -27,41 +28,23 @@ async def async_setup_entry(
     """Set up the Tecnosystemi climate entities from a config entry."""
     api = entry.runtime_data
 
-    # We find all plants from the user, and the relevant plants. Right now,
-    # the are all added as climate entities.
+    coordinator = TecnosystemiCoordinator(hass, entry, api)
+
+    await coordinator.async_config_entry_first_refresh()
+
     entities = []
-    for plant in await api.GetPlants():
-        # print(f"Found plant: {plant.LVPL_Name} with ID: {plant.LVPL_Id}")
-
-        for device in plant.getDevices():
-            # print("  Device:", device.Name, "Serial:", device.Serial)
-
-            # To actually find the zone thermostats, we need to get the state;
-            # Then, we create single climate entities for each of them, with
-            # a single parent device (the Polaris controller).
-            state = await api.getDeviceState(device, entry.data[CONF_PIN])
-            # print("  State:", json.dumps(state))
-
-            # Sample output for state:
-            # {"Zones": [
-            #    {"ZoneId": 1, "Name": "SALOTTO", "IsMaster": true, "IsOFF": true, "Temp": "265", "SetTemp": "260", "Serranda": 0, "SerrandaSet": 0, "Fancoil": -1, "FancoilSet": 0, "EV": -1, "IsCronoMode": false, "IsCronoActive": true, "Errors": 0, "Umd": "594", "SetUmd": null, "CWin": -1, "CBadge": -1, "COff": false},
-            #    {"ZoneId": 2, "Name": "CUCINA", "IsMaster": false, "IsOFF": true, "Temp": "266", "SetTemp": "260", "Serranda": 0, "SerrandaSet": 0, "Fancoil": -1, "FancoilSet": -1, "EV": -1, "IsCronoMode": false, "IsCronoActive": false, "Errors": 0, "Umd": "603", "SetUmd": null, "CWin": -1, "CBadge": -1, "COff": false},
-            #    {"ZoneId": 3, "Name": "CAMERETTA", "IsMaster": false, "IsOFF": true, "Temp": "265", "SetTemp": "220", "Serranda": 0, "SerrandaSet": 0, "Fancoil": -1, "FancoilSet": -1, "EV": -1, "IsCronoMode": false, "IsCronoActive": false, "Errors": 0, "Umd": "599", "SetUmd": null, "CWin": -1, "CBadge": -1, "COff": false},
-            #    {"ZoneId": 4, "Name": "STUDIO", "IsMaster": false, "IsOFF": false, "Temp": "263", "SetTemp": "210", "Serranda": 19, "SerrandaSet": 0, "Fancoil": -1, "FancoilSet": -1, "EV": -1, "IsCronoMode": false, "IsCronoActive": true, "Errors": 0, "Umd": "565", "SetUmd": null, "CWin": -1, "CBadge": -1, "COff": false},
-            #    {"ZoneId": 5, "Name": "CAMERA", "IsMaster": false, "IsOFF": true, "Temp": "265", "SetTemp": "210", "Serranda": 0, "SerrandaSet": 0, "Fancoil": -1, "FancoilSet": -1, "EV": -1, "IsCronoMode": false, "IsCronoActive": false, "Errors": 0, "Umd": "590", "SetUmd": null, "CWin": -1, "CBadge": -1, "COff": false}], "Errors": 0, "Serial": "414705038650", "Name": "CLIMATIZZATORE", "FWVer": "5.0.15", "IsOFF": false, "IsCooling": true, "OperatingModeCooling": 1, "LastConfigUpdate": "2025-07-26T13:53:32.58", "LastSyncUpdate": "2025-07-26T14:24:43.983", "NumErrors": 0, "Icon": 0, "IrPresent": 1, "TempCan": "230", "IP": "", "FInv": 1, "FEst": 1}
-            # ]}
-            for zone in state["Zones"]:
-                # print("  Zone:", zone["Name"], "ID:", zone["ZoneId"])
-
-                entity = TecnosystemiClimateEntity(
-                    device=device, config=entry, zone=zone
-                )
-                entities.append(entity)
+    for device_id in coordinator.data:
+        entity = TecnosystemiClimateEntity(
+            device_id=device_id,
+            zone=coordinator.data[device_id],
+            coordinator=coordinator,
+        )
+        entities.append(entity)
 
     async_add_entities(entities)
 
 
-class TecnosystemiClimateEntity(ClimateEntity):
+class TecnosystemiClimateEntity(CoordinatorEntity, ClimateEntity):
     """Minimal Climate entity for Tecnosystemi integration."""
 
     _attr_has_entity_name = True
@@ -72,14 +55,17 @@ class TecnosystemiClimateEntity(ClimateEntity):
     _attr_hvac_action = None  # Could be none, "heating", "cooling"
 
     def __init__(
-        self, device: Device, config: TecnosystemiConfigEntry, zone: dict
+        self, device_id: str, zone: dict, coordinator: TecnosystemiCoordinator
     ) -> None:
         """Initialize the climate entity."""
+        CoordinatorEntity.__init__(self, coordinator)
+
         self.zone_state = zone
+        self.device_id = device_id
+        self._attr_unique_id = device_id
+        self.coordinator = coordinator
 
-        self._attr_unique_id = device.Serial + "_" + str(zone["ZoneId"])
-
-        self._attr_name = zone["Name"] + " - " + device.Name
+        self._attr_name = zone["Name"] + " - " + zone["Device"].Name
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, self._attr_unique_id)},
             name=self._attr_name,
@@ -87,19 +73,30 @@ class TecnosystemiClimateEntity(ClimateEntity):
             model="ProAir",
         )
 
-    @property
-    def current_temperature(self) -> float | None:
-        """Return the current temperature."""
-        if "Temp" in self.zone_state:
-            return float(self.zone_state["Temp"]) / 10.0
-        return None
+        # self._handle_coordinator_update()
+        self.update_attrs_from_state()
 
-    @property
-    def target_temperature(self) -> float | None:
-        """Return the temperature we try to reach."""
-        if "SetTemp" in self.zone_state:
-            return float(self.zone_state["SetTemp"]) / 10.0
-        return None
+    def update_attrs_from_state(self):
+        """Update attributes from the current state."""
+        self._attr_hvac_mode = (
+            HVACMode.OFF if self.zone_state["IsOFF"] else HVACMode.COOL
+        )
+        self._attr_current_temperature = float(self.zone_state["Temp"]) / 10.0
+        self._attr_target_temperature = float(self.zone_state["SetTemp"]) / 10.0
+        self._attr_current_humidity = float(self.zone_state.get("Umd", 0)) / 10.0
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        # print(self._attr_unique_id)
+        # print(self.coordinator.data)
+        self.zone_state = self.coordinator.data[self.device_id]
+        # print("Zone: ", self.zone_state["Name"])
+        # print("Updated zone state:", self.zone_state)
+
+        self.update_attrs_from_state()
+        # self.state = self.coordinator.data[self.zone["ZoneId"]]
+        self.async_write_ha_state()
 
     async def async_set_temperature(self, **kwargs: Any) -> None:
         """Set new target temperature."""
@@ -109,5 +106,6 @@ class TecnosystemiClimateEntity(ClimateEntity):
     async def async_set_hvac_mode(self, hvac_mode: HVACMode) -> None:
         """Set new HVAC mode."""
         # Implement setting HVAC mode here
-        self._attr_hvac_mode = hvac_mode
-        self.async_write_ha_state()
+        self._attr_hvac_mode = hvac_mode  # We actually need to this through APIs
+        await self.coordinator.async_request_refresh()
+        # self.async_write_ha_state()
---
+++ homeassistant/components/tecnosystemi/coordinator.py
@@ -0,0 +1,96 @@
+"""Coordinator for Tecnosystemi integration."""
+
+from __future__ import annotations
+
+import asyncio
+from datetime import timedelta
+import logging
+
+from homeassistant.const import CONF_PIN
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class TecnosystemiCoordinator(DataUpdateCoordinator):
+    """My custom coordinator."""
+
+    def __init__(self, hass, config_entry, api):
+        """Initialize my coordinator."""
+        super().__init__(
+            hass,
+            _LOGGER,
+            # Name of the data. For logging purposes.
+            name="Tecnosystemi Climate Coordinator",
+            config_entry=config_entry,
+            # Polling interval. Will only be polled if there are subscribers.
+            update_interval=timedelta(seconds=30),
+            # Set always_update to `False` if the data returned from the
+            # api can be compared via `__eq__` to avoid duplicate updates
+            # being dispatched to listeners
+            always_update=True,
+        )
+        self.api = api
+        self._plants = []
+
+    async def _async_setup(self):
+        """Set up the coordinator.
+
+        This is the place to set up your coordinator,
+        or to load data, that only needs to be loaded once.
+
+        This method will be called automatically during
+        coordinator.async_config_entry_first_refresh.
+        """
+        # self._device = await self.my_api.get_device()
+        self._plants = await self.api.GetPlants()
+
+    async def _async_update_data(self):
+        """Fetch data from API endpoint.
+
+        This is the place to pre-process the data to lookup tables
+        so entities can quickly look up their data.
+        """
+        data = {}
+
+        try:
+            # Note: asyncio.TimeoutError and aiohttp.ClientError are already
+            # handled by the data update coordinator.
+            async with asyncio.timeout(10):
+                # Grab active context variables to limit data required to be fetched from API
+                # Note: using context is not required if there is no need or ability to limit
+                # data retrieved from API.
+                # listening_idx = set(self.async_contexts())
+                for plant in self._plants:
+                    # print(f"Found plant: {plant.LVPL_Name} with ID: {plant.LVPL_Id}")
+
+                    for device in plant.getDevices():
+                        # print("  Device:", device.Name, "Serial:", device.Serial)
+
+                        # To actually find the zone thermostats, we need to get the state;
+                        # Then, we create single climate entities for each of them, with
+                        # a single parent device (the Polaris controller).
+                        state = await self.api.getDeviceState(
+                            device, self.config_entry.data[CONF_PIN]
+                        )
+                        # print("  State:", json.dumps(state))
+
+                        # Sample output for state:
+                        # {"Zones": [
+                        #    {"ZoneId": 1, "Name": "SALOTTO", "IsMaster": true, "IsOFF": true, "Temp": "265", "SetTemp": "260", "Serranda": 0, "SerrandaSet": 0, "Fancoil": -1, "FancoilSet": 0, "EV": -1, "IsCronoMode": false, "IsCronoActive": true, "Errors": 0, "Umd": "594", "SetUmd": null, "CWin": -1, "CBadge": -1, "COff": false},
+                        #    {"ZoneId": 2, "Name": "CUCINA", "IsMaster": false, "IsOFF": true, "Temp": "266", "SetTemp": "260", "Serranda": 0, "SerrandaSet": 0, "Fancoil": -1, "FancoilSet": -1, "EV": -1, "IsCronoMode": false, "IsCronoActive": false, "Errors": 0, "Umd": "603", "SetUmd": null, "CWin": -1, "CBadge": -1, "COff": false},
+                        #    {"ZoneId": 3, "Name": "CAMERETTA", "IsMaster": false, "IsOFF": true, "Temp": "265", "SetTemp": "220", "Serranda": 0, "SerrandaSet": 0, "Fancoil": -1, "FancoilSet": -1, "EV": -1, "IsCronoMode": false, "IsCronoActive": false, "Errors": 0, "Umd": "599", "SetUmd": null, "CWin": -1, "CBadge": -1, "COff": false},
+                        #    {"ZoneId": 4, "Name": "STUDIO", "IsMaster": false, "IsOFF": false, "Temp": "263", "SetTemp": "210", "Serranda": 19, "SerrandaSet": 0, "Fancoil": -1, "FancoilSet": -1, "EV": -1, "IsCronoMode": false, "IsCronoActive": true, "Errors": 0, "Umd": "565", "SetUmd": null, "CWin": -1, "CBadge": -1, "COff": false},
+                        #    {"ZoneId": 5, "Name": "CAMERA", "IsMaster": false, "IsOFF": true, "Temp": "265", "SetTemp": "210", "Serranda": 0, "SerrandaSet": 0, "Fancoil": -1, "FancoilSet": -1, "EV": -1, "IsCronoMode": false, "IsCronoActive": false, "Errors": 0, "Umd": "590", "SetUmd": null, "CWin": -1, "CBadge": -1, "COff": false}], "Errors": 0, "Serial": "414705038650", "Name": "CLIMATIZZATORE", "FWVer": "5.0.15", "IsOFF": false, "IsCooling": true, "OperatingModeCooling": 1, "LastConfigUpdate": "2025-07-26T13:53:32.58", "LastSyncUpdate": "2025-07-26T14:24:43.983", "NumErrors": 0, "Icon": 0, "IrPresent": 1, "TempCan": "230", "IP": "", "FInv": 1, "FEst": 1}
+                        # ]}
+                        for zone in state["Zones"]:
+                            # print("  Zone:", zone["Name"], "ID:", zone["ZoneId"])
+                            zone["Device"] = device
+                            zone["Plant"] = plant
+                            data[
+                                f"{plant.LVPL_Id}_{device.Serial}_{zone['ZoneId']}"
+                            ] = zone
+
+                    return data
+        except RuntimeError as err:
+            raise UpdateFailed(f"Error communicating with API: {err}") from None
