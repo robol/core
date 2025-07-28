052c9d860e654db51f3141ebd6cf0e001d9085dc
Leonardo Robol <leo@robol.it>
2025-07-28 12:27:53 +0200

    Better labels for sensors.

--- homeassistant/components/tecnosystemi/sensor.py
+++ homeassistant/components/tecnosystemi/sensor.py
@@ -103,7 +103,7 @@ class TecnosystemiTemperatureSensorEntity(TecnosystemiSensorEntity):
         TecnosystemiSensorEntity.__init__(self, device_id, zone, coordinator, api, pin)
 
         self._attr_unique_id = device_id + "_temperature"
-        self._attr_name = zone["Name"] + " - " + zone["Device"].Name
+        self._attr_name = "Temperature of " + zone["Name"] + " - " + zone["Device"].Name
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, self._attr_unique_id)},
             name=self._attr_name,
@@ -137,7 +137,7 @@ class TecnosystemiHumiditySensorEntity(TecnosystemiSensorEntity):
         TecnosystemiSensorEntity.__init__(self, device_id, zone, coordinator, api, pin)
 
         self._attr_unique_id = device_id + "_humidity"
-        self._attr_name = zone["Name"] + " - " + zone["Device"].Name
+        self._attr_name = "Humidity of " + zone["Name"] + " - " + zone["Device"].Name
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, self._attr_unique_id)},
             name=self._attr_name,
